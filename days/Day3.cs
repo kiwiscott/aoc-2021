90f392c78f9e7eb31e6cb7a6a81115b4df4b72fe
kiwiscott <you@example.com>
2021-12-03 12:34:21 -0500

    Day 3

---
+++ days/Day3.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+namespace aoc.Days;
+
+public class Day3
+{
+    public static object Data(string file)
+    {
+        var d = Lib.Load<BitArray>(file, convert);
+        return d;
+    }
+    public static Func<string?, BitArray> convert = delegate (string? s)
+    {
+        var parts = s.ToCharArray().Select(p => p == '1').ToArray();
+        return new BitArray(parts);
+    };
+
+
+    public static int Part1(List<BitArray> data)
+    {
+        //Whats the length of the BitArrays
+        var gamma_array = new BitArray(data[0].Length);
+        var epsilon_array = new BitArray(data[0].Length);
+
+        for (int i = 0; i < data[0].Length; i++)
+        {
+            var position = msb_at_positon(data, i);
+            gamma_array.Set(i, position);
+            epsilon_array.Set(i, !position);
+        }
+
+        var gamma_rate = bits_to_int(gamma_array);
+        var epsilon_rate = bits_to_int(epsilon_array);
+
+        return gamma_rate * epsilon_rate;
+    }
+    public static int Part2(List<BitArray> data)
+    {
+        var ogr_ba = find_one(data, true);
+        var oxygen_generator_rating = bits_to_int(ogr_ba);
+
+        var c02_scrubber_ba = find_one(data, false);
+        var co2_scrubber_rating = bits_to_int(c02_scrubber_ba);
+
+        return co2_scrubber_rating * oxygen_generator_rating;
+    }
+
+    static string bits_to_string(BitArray ba)
+    {
+
+        var s = "";
+        foreach (bool b in ba)
+        {
+            s += b ? "1" : "0";
+        }
+        return s;
+    }
+    static int bits_to_int(BitArray ba)
+    {
+        var s = bits_to_string(ba);
+        return Convert.ToInt32(s, 2);
+    }
+
+    static BitArray find_one(List<BitArray> data, bool msb)
+    {
+        var dcurrent = data.ToList();
+        var current_index = 0;
+
+        while (true)
+        {
+            var sig_bit = msb ? msb_at_positon(dcurrent, current_index) : lsb_at_positon(dcurrent, current_index);
+
+            dcurrent = dcurrent.Where(p => p[current_index] == sig_bit).ToList();
+            current_index += 1;
+
+
+            if (dcurrent.Count() == 1)
+            {
+                return dcurrent.First();
+            }
+        }
+    }
+
+    static bool msb_at_positon(List<BitArray> ba, int postion)
+    {
+        return ba.Where(p => p[postion]).Count() >= ba.Where(p => !p[postion]).Count();
+    }
+    static bool lsb_at_positon(List<BitArray> ba, int postion)
+    {
+        return ba.Where(p => p[postion]).Count() < ba.Where(p => !p[postion]).Count();
+    }
+
+}
