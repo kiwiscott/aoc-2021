fca4d92d1146a9d171fa84f537ae647751f90e60
kiwiscott <you@example.com>
2021-12-05 20:13:57 -0500

    Added Run all days

--- Lib.cs
+++ Lib.cs
@@ -1,11 +1,9 @@
-using System;
-using System.IO;
 using System.Reflection;
 
 namespace aoc;
 public static class Lib
 {
-    public static List<T> LoadList<T>(string path, Func<string?, T> process)
+    public static List<T> LoadList<T>(string path, Func<string, T> process)
     {
         var l = new List<T>();
 
@@ -15,7 +13,7 @@ public static class Lib
             string? s;
             while ((s = sr.ReadLine()) != null)
             {
-                l.Add(process(s));
+                if (s is not null) l.Add(process(s));
             }
         }
         return l;
@@ -37,73 +35,4 @@ public static class Lib
     }
 
 
-    internal static Tuple<string, bool, bool> ProcessArgs(string[] args)
-    {
-        string typeName = "aoc.Days.Day" + DateTime.Now.Day;
-
-        bool runPart1 = true;
-        bool runPart2 = true;
-
-        for (int i = 0; i < args.Length; i++)
-        {
-            if (args[i] == "-part" && args.GetValue(i + 1) is not null)
-            {
-                runPart1 = args[i + 1] == "1";
-                runPart2 = args[i + 1] == "2";
-            }
-            else if (args[i] == "-day" && args.GetValue(i + 1) is not null)
-            {
-                typeName = "aoc.Days.Day" + args[i+1];
-            }
-        }
-        return Tuple.Create(typeName, runPart1, runPart2);
-    }
-
-    internal static void RunMethod(string type, string method)
-
-    {
-        var t = Type.GetType(type);
-
-        if (t is null)
-        {
-            Console.WriteLine("Type {0} not found", type);
-            return;
-        }
-
-        var constr = t.GetConstructor(System.Type.EmptyTypes); 
-        var o = constr.Invoke(null); 
-
-        object? data = null;
-
-        var dataMethod = t.GetMethod("Data");
-        if (dataMethod is not null)
-        {
-            string filename = String.Format(@"data/{0}.txt", t.Name.ToLower());
-            data = dataMethod.Invoke(o, new object[] { filename });
-        }
-        else
-        {
-
-            Console.WriteLine("{0}: Missing Data Function", t.Name);
-            return;
-        }
-
-
-        var builderMethod = t.GetMethod(method);
-
-        if (builderMethod is not null)
-        {
-            var watch = System.Diagnostics.Stopwatch.StartNew();
-
-
-            var result = builderMethod.Invoke(o, new object[] { data });
-            watch.Stop();
-            Console.WriteLine("{0}.{1} : {2} in {3}ms", t.Name, method, result, watch.ElapsedMilliseconds);
-        }
-        else
-        {
-            Console.WriteLine("{0}.{1} : Missing", t.Name, method);
-        }
-
-    }
 }
